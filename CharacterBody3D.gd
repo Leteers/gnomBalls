e358a158acad128cce525024b6640543fe750b11
dudenss <rkamushek@gmail.com>
2024-07-09 00:18:11 +0700

    jump added

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -5,6 +5,7 @@ extends CharacterBody3D
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
+@export var jump_force = 20
 var target_velocity = Vector3.ZERO
 
 
@@ -23,14 +24,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		direction.x -= sqrt(2)
 		direction.z -= sqrt(2)
-
+	if is_on_floor():
+		if Input.is_action_pressed("jump_up"):
+			direction.y += 200
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
+	if is_on_floor():
+		target_velocity.y = direction.y * jump_force
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
